0f0c7eca85fcaf24d6c3eb8ad2b1ad7b8fd521ea
Abdelrahman elghamry <abdelrahmanelghamry013@gmail.com>
2022-08-30 11:17:36 +0200

    Creation

---
+++ ServerUDP/MainWindow.xaml.cs
@@ -0,0 +1,574 @@
+﻿using DevExpress.Xpf.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Threading;
+
+
+namespace Server
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ThemedWindow
+
+    {
+        UdpServerWithSocket test,test1,test2;
+        public int messageCounter  = 0;
+        public int messageLimit = 90;
+        bool start_flag,start_flag1,start_flag2 = false;
+        String message, message1 , message2 , message3 , message4 , message5 , message6 , message7 , message8 = "";
+        int freq , freq1 , freq2 , freq3 , freq4 , freq5 , freq6 , freq7 , freq8 = 0;
+        Thread sendMsgThread, sendMsgThread1, sendMsgThread2, sendMsgThread3, sendMsgThread4, sendMsgThread5, sendMsgThread6, sendMsgThread7, sendMsgThread8;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Restart()
+        {
+                Stop();
+                Start();
+      
+        
+         
+
+
+
+        }
+
+        private void Restart1()
+        {
+        
+        
+                Stop1();
+                Start1();
+            
+      
+
+
+
+        }
+
+        private void Restart2()
+        {
+      
+           
+                Stop2();
+                Start2();
+           
+
+
+
+        }
+
+        //private void Restart()
+        //{
+
+        //    Stop();
+        //    Start();
+
+
+        //}
+
+
+
+        private void DisableEt(int field_no)
+        {
+            if(field_no == 0)
+            {
+                etFreq.IsEnabled = false;
+                etPort.IsEnabled = false;
+                etMessage.IsEnabled = false;
+            }
+
+            if (field_no == 1)
+            {
+                etFreq1.IsEnabled = false;
+                etPort1.IsEnabled = false;
+                etMessage1.IsEnabled = false;
+            }
+
+            if (field_no == 2)
+            {
+                etFreq2.IsEnabled = false;
+                etPort2.IsEnabled = false;
+                etMessage2.IsEnabled = false;
+            }
+
+        }
+
+        private void EnableEt(int field_no)
+        {
+
+
+            if (field_no == 0)
+            {
+                etFreq.IsEnabled = true;
+                etPort.IsEnabled = true;
+                etMessage.IsEnabled = true;
+            }
+
+
+            if (field_no == 1)
+            {
+                etFreq1.IsEnabled = true;
+                etPort1.IsEnabled = true;
+                etMessage1.IsEnabled = true;
+            }
+
+
+            if (field_no == 2)
+            {
+                etFreq2.IsEnabled = true;
+                etPort2.IsEnabled = true;
+                etMessage2.IsEnabled = true;
+            }
+
+
+        }
+
+        private void startClicked(object sender, RoutedEventArgs e)
+
+        {
+            
+            if (!start_flag)
+            {
+                Start();
+            }
+            else
+            {
+
+                if (test != null)
+                    test.isClientConnected = false;
+         
+                Stop();
+  
+           
+
+            }
+        }
+
+        private void startClicked1(object sender, RoutedEventArgs e)
+
+        {
+            if (!start_flag1)
+            {
+                Start1();
+            }
+            else
+            {
+
+                if (test1 != null)
+                    test1.isClientConnected = false;
+
+                Stop1();
+
+
+
+            }
+        }
+
+
+
+        private void startClicked2(object sender, RoutedEventArgs e)
+
+        {
+
+            
+            if (!start_flag2)
+            {
+                Start2();
+            }
+            else
+            {
+
+                if (test2 != null)
+                    test.isClientConnected = false;
+
+                Stop2();
+
+
+
+            }
+        }
+
+
+
+        
+
+        public void sendMessageWithFreq(String message , int freq,UdpServerWithSocket test ,bool start_flag)
+        {
+
+            int i = 0;
+
+            while (true)
+            {
+
+
+                try
+                {
+                    //   string tt = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"); ;
+
+                    if (start_flag)
+                    {
+                        if(test!=null)
+                        if ( message != "" && freq != 0 && message != null && test.isClientConnected)
+                        //test.sendMessage(test.message);
+                        {
+                            test.sendMessage(message +" "+ i.ToString() +" " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+                            i++;
+                            if(test!= null)
+                            if (test.isClientConnected)
+                                Thread.Sleep(freq);
+                        }
+
+                    }
+
+                }
+                catch (SocketException se)
+                {
+
+
+
+                }
+
+            }
+        }
+
+        private void Start()
+        {
+
+            
+
+         
+                
+            this.Dispatcher.Invoke(() =>
+            {
+                EnableEt(0);
+                if (!etPort.IsNullTextVisible && !etFreq.IsNullTextVisible && !etMessage.IsNullTextVisible)
+                {
+                    test = new UdpServerWithSocket(int.Parse(etPort.Text.ToString()), int.Parse(etFreq.Text.ToString()), etMessage.Text);
+                    test.ClientDisConnectedEvent += Restart;
+                    test.MessageSentCounterEvent += CountMessages;
+
+                    if (!etMessage.IsNullTextVisible)
+                    message = etMessage.Text;
+               
+          
+
+                    if(!etFreq.IsNullTextVisible)
+                    freq = int.Parse(etFreq.Text.ToString());
+            
+
+
+
+                    startBtn.Content = "Stop";
+                    DisableEt(0);
+
+                    start_flag = true;
+                    
+
+                }
+
+            });
+
+            messageCounter = 0;
+
+            //here is messages with frequ
+            //commented 25-7-2002 10AM // backtoit
+            if (message != "" && freq != 0)
+            {
+                sendMsgThread = new Thread(() => sendMessageWithFreq(message, freq,test,start_flag));
+                sendMsgThread.Start();
+            }
+
+
+
+
+
+        }
+
+
+
+        private void Start1()
+        {
+
+
+
+
+
+            this.Dispatcher.Invoke(() =>
+            {
+                EnableEt(1);
+                if (!etPort1.IsNullTextVisible && !etFreq1.IsNullTextVisible && !etMessage1.IsNullTextVisible)
+                {
+                    test1 = new UdpServerWithSocket(int.Parse(etPort1.Text.ToString()), int.Parse(etFreq1.Text.ToString()), etMessage1.Text);
+                    test1.ClientDisConnectedEvent += Restart1;
+                    test1.MessageSentCounterEvent += CountMessages;
+
+             
+                    if (!etMessage1.IsNullTextVisible)
+                        message1 = etMessage1.Text;
+           
+
+
+                   
+                    if (!etFreq1.IsNullTextVisible)
+
+                        freq1 = int.Parse(etFreq1.Text.ToString());
+           
+
+
+
+
+
+                    startBtn1.Content = "Stop";
+                    DisableEt(1);
+
+                    start_flag1 = true;
+
+
+                }
+
+            });
+
+            messageCounter = 0;
+
+            //here is messages with frequ
+            //commented 25-7-2002 10AM // backtoit
+  
+
+
+            if (message1 != "" && freq1 != 0)
+            {
+
+
+                sendMsgThread1 = new Thread(() => sendMessageWithFreq(message1, freq1,test1,start_flag1));
+                sendMsgThread1.Start();
+            }
+
+    
+
+
+        }
+
+
+
+        private void Start2()
+        {
+
+
+
+
+
+            this.Dispatcher.Invoke(() =>
+            {
+                EnableEt(2);
+                if (!etPort2.IsNullTextVisible && !etFreq2.IsNullTextVisible && !etMessage2.IsNullTextVisible)
+                {
+                    test2 = new UdpServerWithSocket(int.Parse(etPort2.Text.ToString()), int.Parse(etFreq2.Text.ToString()), etMessage2.Text);
+                    test2.ClientDisConnectedEvent += Restart2;
+                    test2.MessageSentCounterEvent += CountMessages;
+
+                  
+                    if (!etMessage2.IsNullTextVisible)
+
+                        message2 = etMessage2.Text;
+
+
+               
+                    if (!etFreq2.IsNullTextVisible)
+
+                        freq2 = int.Parse(etFreq2.Text.ToString());
+
+
+
+
+
+                    startBtn2.Content = "Stop";
+                    DisableEt(2);
+
+                    start_flag2 = true;
+
+
+                }
+
+            });
+
+            messageCounter = 0;
+
+      
+
+            if (message2 != "" && freq2 != 0)
+            {
+
+                sendMsgThread2 = new Thread(() => sendMessageWithFreq(message2, freq2,test2,start_flag2));
+                sendMsgThread2.Start();
+
+            }
+
+
+        }
+
+
+
+        private void CountMessages()
+        {
+
+            messageCounter++;
+
+
+
+        }
+
+        private void Stop()
+        {
+
+            try
+            {
+                if (test != null)
+                    test.stopReceive();
+               
+                
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    startBtn.Content = "Start";
+                    EnableEt(0);
+                    start_flag = false;
+                });
+
+          
+                if (sendMsgThread != null)
+                {
+                    sendMsgThread.Abort();
+
+                    sendMsgThread = null;
+                    test = null;
+                }
+
+               
+
+
+
+            }
+            catch (Exception e)
+            {
+
+
+            }
+
+           
+        }
+
+
+        private void Stop1()
+        {
+
+            try
+            {
+                if (test1 != null)
+                    test1.stopReceive();
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    startBtn1.Content = "Start";
+                    EnableEt(1);
+                    start_flag1 = false;
+                });
+
+
+        
+
+                if (sendMsgThread1 != null)
+                {
+                    sendMsgThread1.Abort();
+
+                    sendMsgThread1 = null;
+                    test1 = null;
+
+                }
+
+
+
+
+            }
+            catch (Exception e)
+            {
+
+
+            }
+
+
+        }
+
+
+
+        private void Stop2()
+        {
+
+            try
+            {
+                if (test2 != null)
+                    test2.stopReceive();
+                
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    startBtn2.Content = "Start";
+                    EnableEt(2);
+                    start_flag2 = false;
+                });
+
+
+
+
+                if (sendMsgThread2 != null)
+                {
+                    sendMsgThread2.Abort();
+
+                    sendMsgThread2 = null;
+                    test2 = null;
+                }
+
+
+
+
+            }
+            catch (Exception e)
+            {
+
+
+            }
+
+
+        }
+
+
+
+
+
+        private void ThemedWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //close any connection
+            //stop listening for server
+            //abort sending thread
+               if(test!=null)
+            test.stopReceive();        
+        }
+    }
+}
---
+++ ServerUDP/UdpServerWithSocket.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Diagnostics;
+
+
+
+namespace Server
+{
+    public class UdpServerWithSocket
+    {
+
+        public delegate void ClientDisConnected();
+        public event ClientDisConnected ClientDisConnectedEvent;
+        public delegate void MessageSentCounter();
+        public event MessageSentCounter MessageSentCounterEvent;
+        public int sampleUdpPort = 6500;
+        public int freq = 0;
+        public String message = "";
+        public bool isClientConnected = false;
+        public Thread sampleUdpThread;
+           Socket soUdp = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+       
+
+        EndPoint remoteEP;
+        IPHostEntry localHostEntry;
+
+
+        public UdpServerWithSocket(int portNo, int freq, String message)
+        {
+            sampleUdpPort = portNo;
+            this.freq = freq;
+            this.message = message;
+            
+
+            try
+            {
+
+                //Starting the UDP Server thread.
+                sampleUdpThread = new Thread(new ThreadStart(startReceive));
+                sampleUdpThread.Start();
+                Debug.WriteLine("Started SampleTcpUdpServer's UDP Receiver Thread!\n");
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("An UDP Exception has occurred!" + e.ToString());
+                sampleUdpThread.Abort();
+            }
+
+            try
+            {
+                //Starting the UDP Server thread.
+                sampleUdpThread = new Thread(new ThreadStart(startReceive));
+                sampleUdpThread.Start();
+                Debug.WriteLine("Started SampleTcpUdpServer's UDP Receiver Thread!\n");
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("An UDP Exception has occurred!" + e.ToString());
+                sampleUdpThread.Abort();
+            }
+
+
+
+
+        }
+
+
+        public void startReceive()
+        {
+
+            try
+            {
+                //Create a UDP socket.
+
+                try
+                {
+                    localHostEntry = Dns.GetHostByName(Dns.GetHostName());
+                }
+                catch (Exception)
+                {
+                    Debug.WriteLine("Local Host not found"); // fail
+                    return;
+                }
+                //initate server listen on port .....
+                IPEndPoint localIpEndPoint = new IPEndPoint(localHostEntry.AddressList[0], sampleUdpPort);
+                soUdp.Bind(localIpEndPoint);
+                Byte[] received = new Byte[4];
+                IPEndPoint tmpIpEndPoint = new IPEndPoint(IPAddress.Any, sampleUdpPort);
+                remoteEP = (tmpIpEndPoint);
+                //while (true)
+                //{
+                //isClientConnected = false;
+                //waiting for client
+                //   int bytesReceived = soUdp.ReceiveFrom(received, ref remoteEP);
+
+
+                // fill out the event arguments and callback method
+                var rxEventArgs = new SocketAsyncEventArgs();
+                rxEventArgs.RemoteEndPoint = remoteEP;
+                //4
+                rxEventArgs.SetBuffer(new byte[2048], 0, 2048);
+                rxEventArgs.Completed += UdpReceiveCallback;
+
+                // initiate a receive operation
+                soUdp.ReceiveFromAsync(rxEventArgs);
+
+                // soUdp.ReceiveFrom(received, ref remoteEP);
+
+
+
+
+                //}
+            }
+            catch (SocketException se)
+            {
+                Debug.WriteLine("A Socket Exception has occurred!" + se.ToString());
+            }
+        }
+
+        public void stopReceive()
+        {
+
+            try
+            {
+
+                if (soUdp.IsBound)
+                {
+                    soUdp.Close();
+                    soUdp.Dispose();
+                }
+
+                if (sampleUdpThread != null)
+                    sampleUdpThread.Abort();
+
+            }
+            catch (Exception e)
+            {
+
+            }
+
+
+
+
+
+        }
+
+        public void sendMessage(String msg)
+        {
+
+
+            try
+            {
+
+                if (soUdp != null && soUdp.IsBound && isClientConnected)
+                {
+                    Byte[] returningByte = System.Text.Encoding.ASCII.GetBytes(msg);
+                    soUdp.SendTo(returningByte, remoteEP);
+                    MessageSentCounterEvent();
+
+
+
+
+                }
+            }
+            catch (Exception se)
+            {
+
+
+
+            }
+
+        }
+
+        public void UdpReceiveCallback(object sock, SocketAsyncEventArgs rxArgs)
+        {
+            // the SockReceiver is the original socket that initiated the receive
+            // the rxArgs are the same ones passed in by the original caller
+
+            if (rxArgs.SocketError == SocketError.Success)
+            {
+                // fire an event for asynch processing elsewhere
+
+                string dataReceived = System.Text.Encoding.ASCII.GetString(rxArgs.Buffer);
+                remoteEP = rxArgs.RemoteEndPoint;
+                //client connect onn us && send message to us
+                //Debug.WriteLine("SampleClient is connected through UDP.");
+                //Debug.WriteLine(dataReceived);
+                //notify client with a message that iam okay 
+                //comment 25-7-2022
+                //sendMessage("Connected" + "," + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+                isClientConnected = true;
+                Debug.WriteLine("Shimaa Time " + dataReceived);
+                Debug.WriteLine("Abdelrahman Time" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+
+          
+               
+                
+
+
+                // ReceiveFromAsync returns true if it will notify you in the future
+                var willRaiseEvent = ((Socket)sock).ReceiveFromAsync(rxArgs);
+
+                // if it returns false it means it will not raise a future event because 
+                // it has received a packet already. Since the listener can get a burst
+                // of packets from apps on the localhost, the loop below hands the
+                // processing off to another thread until ReceiveFromAsync finally
+                // returns true and says that it will raise an event in the future
+                // when the next packet arrives.
+                //while (!willRaiseEvent)
+                //{
+                //    // fire an event for asynch processing elsewhere
+                //    dataReceived = System.Text.Encoding.ASCII.GetString(rxArgs.Buffer);
+                //    Debug.Print("Shimaa Time" + dataReceived);
+                //    Debug.Print("Abdelrahman Time" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+                //    //remoteEP = rxArgs.RemoteEndPoint;
+                //    ////client connect onn us && send message to us
+                //    ////Debug.WriteLine("SampleClient is connected through UDP.");
+                //    ////Debug.WriteLine(dataReceived);
+                //    ////notify client with a message that iam okay 
+
+                //    //sendMessage(dataReceived);
+
+
+
+                //    //  ClientDisConnectedEvent();
+                //    // isClientConnected = false;
+                //    //eturn;
+
+
+
+                //}
+            }
+            else
+            {
+               // if (isClientConnected && ! IsStoppedFromServer) ClientDisConnectedEvent();
+                
+                if(isClientConnected == true)
+                {
+
+                    isClientConnected = false;
+                    ClientDisConnectedEvent();
+                   
+
+                }
+                else
+                {
+                    isClientConnected = false;
+                }
+               
+
+            }
+        }
+
+
+    }
+
+
+
+
+}
+
